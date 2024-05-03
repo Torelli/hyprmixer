4de7cdf766ac3fbfa88cddb93b2bbb8536e4aefb
Giovanni Torelli Leite Walter <giovanni.walter@outlook.com>
2024-05-03 17:00:44 -0300

    Minor style change

--- src/components/track/Track.tsx
+++ src/components/track/Track.tsx
@@ -18,13 +18,12 @@ export default function Track() {
 		<button onClick={() => setDiscMode(!isDiscMode)} className={`drop-shadow-md w-28 h-28 sm:w-32 sm:h-32 md:w-44  md:h-44 focus-visible:outline-white ${isDiscMode ? `border-none animate-spin-slow ${isPlaying ? "hover:pause" : "pause"} rounded-full overflow-hidden` : track?.artUrl == "" ? "border hover:scale-110" : "hover:scale-110"} transition-all`}>
 			{track?.artUrl !== "" ? <img src={track?.artUrl} /> : isDiscMode ? <DiscIcon /> : <FontAwesomeIcon className="text-4xl" icon="fa-regular fa-image" />}
 		</button>
-		<div className="flex flex-col gap-4 w-64">
+		<div className="flex flex-col gap-2 w-64">
 			<div className="flex flex-col justify-center items-center">
 				<h1 className="font-bold text-sm">{track?.title}</h1>
 				<h2 className="text-slate-400 text-xs">{track?.artist}</h2>
 			</div>
 			<ProgressBar position={track?.position as string} length={track?.length as string} />
-
 			<div className="min-h-12 sm:min-h-20 text-sm sm:text-lg md:text-2xl flex items-center justify-around">
 				<button onClick={() => prev(currentPlayer)} className="hover:bg-gray-900/60 py-2 px-4 rounded-full focus-visible:outline-white transition-all"><FontAwesomeIcon icon="fa-solid fa-backward-step" /></button>
 				<button onClick={() => togglePlayPause(currentPlayer)} className={`hover:bg-gray-900/60 ${isPlaying ? "py-2 px-4" : "py-3 pl-5 pr-4"} rounded-full focus-visible:outline-white transition-all`}>
